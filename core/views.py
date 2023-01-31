2974a3f3fd68afc100a95bbec26c0bc1cd2535b7
RJ <a0970220839@gmail.com>
2023-01-31 23:46:01 +0800

    login & logout

--- core/views.py
+++ core/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 from core.models import Profile
+
+
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -41,3 +44,27 @@ def signup(request):
         
     else:
         return render(request, 'signup.html')
+    
+    
+    
+def signin(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credential Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
